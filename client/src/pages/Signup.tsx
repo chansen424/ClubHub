0f951ed57681595ff7479373b93d71f8975bd3ee
Christopher Hansen <chansen424@yahoo.com>
2020-09-24 19:49:37 -0400

    Add signup account type selection

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ApplicantSignup from '../components/ApplicantSignup';
+import OrgSignup from '../components/OrgSignUp';
 
 export default function Signup() {
-    return <h1>Signup</h1>
+    const [userType, setUserType] = useState("orgs");
+    return (
+    <>
+        <h1>Signup</h1>
+        <label htmlFor="userType">Signup as:</label>
+        <select name="userType" onChange={e => setUserType(e.target.value)}>
+            <option value="orgs">Organization</option>
+            <option value="applicants">Applicant</option>
+        </select>
+        {userType === "orgs" && <OrgSignup />}
+        {userType === "applicants" && <ApplicantSignup />}
+    </>
+    )
 }
